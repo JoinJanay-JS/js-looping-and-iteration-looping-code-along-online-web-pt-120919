fee8d5c7bc6d6be84423585b870884552363d2f3
Janay Bailey <tjanay87@gmail.com>
2020-06-29 15:05:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // Code your solutions in this file
 writeCards(["Ada", "Brendan", "Ali"], "birthday");
 
+const gifts = ["teddy bear", "drone", "doll"];
+ 
+function wrapGifts(gifts) {
+  let i = 0; // the initialization moved OUTSIDE the body of the loop!
+  while (i < gifts.length) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    i++; // the iteration moves INSIDE the body of the loop!
+  }
+ 
+  return gifts;
+}
+ 
+wrapGifts(gifts);
