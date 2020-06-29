2e49bb7c4a785aa4426e41abfd3f0b970c11c596
Janay Bailey <tjanay87@gmail.com>
2020-06-29 15:08:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ writeCards(["Ada", "Brendan", "Ali"], "birthday");
 
 const cards = 
 
+function writeCards(cards)
+let countup = 0;
+while (countup < 10) {
+  console.log(countup++);
+}
 
+return cards;
+
+}
 
 
 
